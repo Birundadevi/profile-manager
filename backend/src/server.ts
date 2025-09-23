304e9246440b676e48203dc46fb3a8df74d0d8a0
Birundadevi <birundadevi.m28@gmail.com>
2025-09-24 00:25:58 +0400

    temp fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,21 +9,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Define a whitelist for allowed origins
-const whitelist = [
-  'http://localhost:5173', // For local development
-  'https://profile-management-ruddy.vercel.app' // For Vercel production
-];
+// const whitelist = [
+//   'http://localhost:5173', // For local development
+//   'https://profile-management-ruddy.vercel.app' // For Vercel production
+// ];
 
 const corsOptions = {
-  origin: function (origin: string | undefined, callback: (error: Error | null, success: boolean) => void) {
-    // Allow requests if the origin is in the whitelist or if it's a local request with no origin
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'), false);
-    }
-  }
+  origin: 'https://profile-management-ruddy.vercel.app', // Replace with your actual domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // This allows cookies and authorization headers to be sent
 };
+
 
 // Use the cors middleware with the defined options
 app.use(cors(corsOptions));
